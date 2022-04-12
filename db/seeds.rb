d3f5d08b10c10dccbf1a7950961f4432753f4160
Anjali Kumar <anjali.kumar06@gmail.com>
2022-04-12 20:55:04 +0200

    Seed DB with restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# puts('Deleting db..')
+# db:drop
+puts('Deleting all restaurants..')
+Restaurant.destroy_all
+
+puts('Seeding database with 10 restaurants..')
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: Restaurant::CATEGORY.sample)
+end
+puts('Restaurants created')
