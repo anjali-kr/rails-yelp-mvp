2e8bbda0bc001662a4aadcc9ba406026b2cee6f6
Anjali Kumar <anjali.kumar06@gmail.com>
2022-04-14 19:19:37 +0200

    Added controller actions and views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,5 +12,17 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.new(valid_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def valid_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
